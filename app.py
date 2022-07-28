b537e25de21d32c99524eb70d796114c29b45326
baodoze <baodoze@yahoo.com.br>
2022-07-28 23:57:05 +0000

    Aula do dia 28

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask("hello")
@@ -9,14 +10,50 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "pudim"
 
 db = SQLAlchemy(app)
+login = LoginManager(app)
+
+#class Post(db.Model):
+#    __tablename__ = 'posts'
+#    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+#    title = db.Column(db.String(70), nullable=False)
+#    body = db.Column(db.String(500))
+#    author = db.Column(db.String(20))
+#    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+#    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+#class User(UserMixin, db.Model):
+#    __tablename__ = "users"
+#    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+#    email = db.Column(db.String(64), nullable=False, unique=True)
+#    password_hash = db.Column(db.String(128), nullable=False)
+#    posts = db.relationship('Post', backref='author')
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(70), nullable=False)
     body = db.Column(db.String(500))
-    author = db.Column(db.String(20))
     created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
+
+def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 db.create_all()
 
@@ -25,12 +62,24 @@ def index():
     posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
-@app.route("/populate")
-def populate():
-    post1 = Post(title="Post 1", body="Texto do Post", author="Dev")
-    post2 = Post(title="Post 2", body="Texto do Post 2", author="Dev")
-    db.session.add(post1)
-    db.session.add(post2)
-    db.session.commit()
-    return redirect(url_for("index"))
+@app.route('/register', methods=["GET", "POST"])
+def register():
+
+    
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+#@app.route("/populate")
+#def populate():
+#    post1 = Post(title="Post 1", body="Texto do Post", author="Dev")
+#    post2 = Post(title="Post 2", body="Texto do Post 2", author="Dev")
+#    db.session.add(post1)
+#   db.session.add(post2)
+#    db.session.commit()
+#    return redirect(url_for("index"))
+
 
